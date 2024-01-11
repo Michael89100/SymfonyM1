5bc86a04b59918763c76551826ff78a796c8a708
Olivier Perdrix <olivierperdrix@live.fr>
2024-01-11 17:24:13 +0100

    feat: :lipstick: Amélioration du système de flash messages

--- app/assets/flashes.js
+++ app/assets/flashes.js
@@ -1,5 +1,13 @@
-var flashLiveExample = document.getElementById('liveFlash')
+// Path: app/assets/flashes.scss
 
-if (flashLiveExample) {
-    flashLiveExample.classList.add('show');
+// Flash messages
+// Récupère tous les éléments avec la classe .liveFlash puis ajoute la classe .show et supprime la classe .show après 5 secondes
+var flashLiveExamples = document.querySelectorAll('.liveFlash');
+if (flashLiveExamples) {
+    flashLiveExamples.forEach(function (flashLiveExample) {
+        flashLiveExample.classList.add('show');
+        setTimeout(function () {
+            flashLiveExample.classList.remove('show');
+        }, 5000);
+    });
 }
